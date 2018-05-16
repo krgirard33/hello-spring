e575c63cb1ed97e01068aa5e0af8082eea4ca1ab
Ken Girard <ken.girard@gmail.com>
2018-05-15 22:44:20 -0500

    Created helloUrlSegment, which looks for a parameter in the url (localhost:8080/hello/name)

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +35,12 @@ public class HelloController {
     @ResponseBody
     public String helloPost(HttpServletRequest request) {
         String name =  request.getParameter("name");
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value="hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
         return "Hello " + name;
     }
 
